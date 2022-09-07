a21d37d5cf19ccceeb76e253b4ef90651bd3e37c
max <maxime.jalbert.locke@gmail.com>
2022-09-07 14:25:56 +1000

    bugfixes

---
+++ README.md
@@ -0,0 +1,3 @@
+SIGMA
+
+Created by Terrence Lim, Alex Jiang and Maxime Jalbert-Locke
--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: "./index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        filename: "main.js",
         clean: true,
         publicPath: "/"
     },
